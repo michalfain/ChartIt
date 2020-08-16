e59ad254b8e1bbeacf73a047b0240b1b70b0282d
michal fain <michelle.fain@gmail.com>
2020-08-16 12:05:35 +0300

    proof of concept

--- app/src/main/java/com/example/chartit/MainActivity.java
+++ app/src/main/java/com/example/chartit/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.chartit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvAddChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvAddChart = findViewById(R.id.add_chart);
+
+        tvAddChart.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(MainActivity.this, AddChart.class);
+                        startActivity(intent);
+
+                    }
+                }
+        );
+
     }
 }
