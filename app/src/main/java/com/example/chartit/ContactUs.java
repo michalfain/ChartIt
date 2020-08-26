565300165dcb7b14461bde95ea4f714546b0c7a3
michal fain <michelle.fain@gmail.com>
2020-08-26 21:57:01 +0300

    contact us- done!

--- app/src/main/java/com/example/chartit/ContactUs.java
+++ app/src/main/java/com/example/chartit/ContactUs.java
@@ -7,9 +7,10 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class ContactUs extends AppCompatActivity {
-    EditText etName, etEmail, etSubject, etMassage;
+    EditText etName, etEmail, etSubject, etMessage;
     String myEmail = "michelle.fain@gmail.com";
 
     @Override
@@ -19,15 +20,22 @@ public class ContactUs extends AppCompatActivity {
         etName = findViewById(R.id.et_name);
         etEmail = findViewById(R.id.et_email);
         etSubject = findViewById(R.id.et_subject);
-        etMassage = findViewById(R.id.et_massage);
+        etMessage = findViewById(R.id.et_massage);
     }
 
 public void sendEmail(View v){
-    String message = etName.getText().toString() + " " + etMassage.getText().toString() + " "
+        if(!etName.getText().toString().isEmpty() && !etEmail.getText().toString().isEmpty() &&
+        !etSubject.getText().toString().isEmpty() && !etMessage.getText().toString().isEmpty()){
+    String message = etName.getText().toString() + " " + etMessage.getText().toString() + " "
             + etEmail.getText().toString();
-    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("mailto:" + myEmail));
+    Intent intent = new Intent(Intent.ACTION_SEND);
+    intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"michelle.fain@gmail.com"});
     intent.putExtra(Intent.EXTRA_SUBJECT, etSubject.getText().toString());
     intent.putExtra(Intent.EXTRA_TEXT, message);
+    intent.setType("message/rfc822");
     startActivity(intent);
-}
+} else {
+            Toast.makeText(ContactUs.this, "Please enter all fields!", Toast.LENGTH_LONG).show();
+        }
+    }
 }
