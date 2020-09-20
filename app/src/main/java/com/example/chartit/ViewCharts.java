a72aec722c882e22020fe429bc664ec11d5df72e
michal fain <michelle.fain@gmail.com>
2020-09-20 16:20:57 +0300

    shared preferences- not done 2

--- app/src/main/java/com/example/chartit/ViewCharts.java
+++ app/src/main/java/com/example/chartit/ViewCharts.java
@@ -31,6 +31,8 @@ public class ViewCharts extends AppCompatActivity {
     ListView listView;
     static ChartsProvider charts = new Charts();
     String title, verse1, verse2;
+    static ArrayList<String> chartsList = new ArrayList<>();
+
 
 
     @Override
@@ -41,94 +43,94 @@ public class ViewCharts extends AppCompatActivity {
         listView = findViewById(R.id.list_view);
         final SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.chartit"
                 , Context.MODE_PRIVATE);
-                HashSet<String> set2 = (HashSet<String>)sharedPreferences.getStringSet("Chart List", null);
+        HashSet<String> set2 = (HashSet<String>) sharedPreferences.getStringSet("Chart List", null);
 
-        if(set2 != null){
-            for(String title : set2){
+        if (set2 != null) {
+            chartsList.addAll(set2);
+            for(String title : chartsList){
                 charts.addTitle(title);
             }
-        }else {
-            charts.getTitles();
-        }
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putStringSet("Chart List", set2).apply();
-        editor.commit();
+            }
+
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putStringSet("Chart List", set2).apply();
+            editor.commit();
 
 //        sharedPreferences = getSharedPreferences("new chart", Context.MODE_PRIVATE);
 
-        final ListAdapter adapter = new ListAdapter(this, charts.getTitles());
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(ViewCharts.this, AddChart.class);
-                title = charts.getTitle(i);
-                verse1 = charts.getVerse1(i);
-                verse2 = charts.getVerse2(i);
-                intent.putExtra("title", title);
-                intent.putExtra("verse1",verse1);
-                intent.putExtra("verse2", verse2);
-                intent.putExtra("index", i);
-                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.chartit", Context.MODE_PRIVATE);
-                HashSet<String> set = new HashSet<>(charts.getTitles());
-                sharedPreferences.edit().putStringSet("Chart List", set).apply();
+            final ListAdapter adapter = new ListAdapter(this, charts.getTitles());
+            listView.setAdapter(adapter);
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                    Intent intent = new Intent(ViewCharts.this, AddChart.class);
+                    title = charts.getTitle(i);
+                    verse1 = charts.getVerse1(i);
+                    verse2 = charts.getVerse2(i);
+                    intent.putExtra("title", title);
+                    intent.putExtra("verse1", verse1);
+                    intent.putExtra("verse2", verse2);
+                    intent.putExtra("index", i);
+                    SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.chartit", Context.MODE_PRIVATE);
+                    HashSet<String> set = new HashSet<>(charts.getTitles());
+                    sharedPreferences.edit().putStringSet("Chart List", set).apply();
 //                SharedPreferences.Editor editor = sharedPreferences.edit();
 //                editor.putString("title", title);
 //                editor.putString("verse1", verse1);
 //                editor.putString("verse2", verse2);
 //                editor.commit();
-                startActivity(intent);
+                    startActivity(intent);
+                }
+            });
+            listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                @Override
+                public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                    new AlertDialog.Builder(ViewCharts.this)
+                            .setIcon(android.R.drawable.ic_dialog_alert)
+                            .setTitle("Are you sure?")
+                            .setMessage("Do you want to delete this chart?")
+                            .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    charts.removeChart(position);
+                                    adapter.notifyDataSetChanged();
+                                    SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.chartit"
+                                            , Context.MODE_PRIVATE);
+                                    HashSet<String> set = new HashSet<>(charts.getTitles());
+                                    sharedPreferences.edit().putStringSet("Chart List", set).apply();
+                                }
+                            })
+                            .setNegativeButton("NO!", null)
+                            .show();
+                    return true;
+                }
+            });
+
+        }
+
+
+
+        class ListAdapter extends ArrayAdapter<String> {
+
+            Context context;
+            String chartTitle[];
+
+            ListAdapter(Context context, List<String> chartTitle) {
+                super(context, R.layout.row_layout, chartTitle);
+                this.context = context;
+                this.chartTitle = chartTitle.toArray(new String[0]);
             }
-        });
-        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+            @NonNull
             @Override
-            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
-                new AlertDialog.Builder(ViewCharts.this)
-                        .setIcon(android.R.drawable.ic_dialog_alert)
-                        .setTitle("Are you sure?")
-                        .setMessage("Do you want to delete this chart?")
-                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                charts.removeChart(position);
-                                adapter.notifyDataSetChanged();
-                                SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("com.example.chartit"
-                                        , Context.MODE_PRIVATE);
-                                HashSet<String> set = new HashSet<>(charts.getTitles());
-                                sharedPreferences.edit().putStringSet("Chart List", set).apply();
-                            }
-                        })
-                        .setNegativeButton("NO!", null)
-                        .show();
-                return true;
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                View row = layoutInflater.inflate(R.layout.row_layout, parent, false);
+                TextView tvChartName = row.findViewById(R.id.tv_chart_name);
+                tvChartName.setText(charts.getTitles().get(position));
+
+                return row;
             }
-        });
 
-    }
-
-
-    class ListAdapter extends ArrayAdapter<String> {
-
-        Context context;
-        String chartTitle[];
-
-        ListAdapter(Context context, List<String> chartTitle) {
-            super(context, R.layout.row_layout, chartTitle);
-            this.context = context;
-            this.chartTitle = chartTitle.toArray(new String[0]);
         }
-
-        @NonNull
-        @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
-            LayoutInflater layoutInflater = (LayoutInflater) getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            View row = layoutInflater.inflate(R.layout.row_layout, parent, false);
-            TextView tvChartName = row.findViewById(R.id.tv_chart_name);
-            tvChartName.setText(charts.getTitles().get(position));
-
-            return row;
-        }
-
     }
-}
-
